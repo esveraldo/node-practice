b5b6775a4e5b0873b4ae5fe019be58d69e1201ce
esveraldo <esveraldo@hotmail.com>
2019-04-11 21:11:59 -0300

    implementacao do marko js

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,27 +1,22 @@
 module.exports = (app) => {
     app.get('/', (req, res, next) => {
-        res.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Página inicial</h1>
-            </body>
-        </html>
-        `);
+        res.marko(require('../views/home/index.marko'));
     });
     
     app.get('/livros', (req, res, next) => {
-        res.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Página dos livros</h1>
-            </body>
-        </html>
-        `);
+        res.marko(require('../views/list/list.marko')
+        ,{
+            livros : [
+                {
+                    id: 1,
+                    titulo: 'Node basico',
+                },
+                {
+                    id: 2,
+                    titulo: 'Node avancado'
+                }
+            ]
+        }
+        );
     });
 };
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,6 +1,12 @@
+//TODO marko js
+require('marko/node-require').install();
+require('marko/express');
+
+//TODO express js
 const express = require('express');
 const app = express();
 
+//TODO Isolando as rotas
 const rotas = require('../app/rotas/rotas');
 rotas(app);
 
